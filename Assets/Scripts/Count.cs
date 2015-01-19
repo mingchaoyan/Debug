e2deead54fa8ba78e06b60f59e24a381baa5e2e8
mingchaoyan <mingchaoyan@gmail.com>
2015-01-19 21:00:42 +0800

    第19章脚本调试与优化

---
+++ Assets/Scripts/Count.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Count : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		int sum = 0;
+		if (GUILayout.Button ("Count")) {
+			for(int i = 0; i < 5; ++i) {
+				sum += i;
+				print (sum);
+			}
+				
+		}
+	}
+}
